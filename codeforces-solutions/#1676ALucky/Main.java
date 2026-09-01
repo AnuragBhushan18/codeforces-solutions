7a2e2f0d6497c2a3c6d50635ef11e5591d3d867a
Anurag Bhushan <bhushan.anurag22@gmail.com>
2026-09-01 21:04:39 +0530

    Solve Codeforces Lucky problem

---
+++ codeforces-solutions/#1676ALucky/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            String s = sc.next();
+
+            int first = (s.charAt(0) - '0')
+                      + (s.charAt(1) - '0')
+                      + (s.charAt(2) - '0');
+
+            int last = (s.charAt(3) - '0')
+                     + (s.charAt(4) - '0')
+                     + (s.charAt(5) - '0');
+
+            if (first == last) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+        sc.close();
+    }
+}
