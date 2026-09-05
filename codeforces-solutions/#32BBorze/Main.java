237283a1abb3871caf2d7eec3e523b0cedd97af1
Anurag Bhushan <bhushan.anurag22@gmail.com>
2026-09-05 17:28:54 +0530

    Solve Borze

---
+++ codeforces-solutions/#32BBorze/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '.') {
+                ans.append('0');
+            } else {
+                i++;
+
+                if (s.charAt(i) == '.') {
+                    ans.append('1');
+                } else {
+                    ans.append('2');
+                }
+            }
+        }
+
+        System.out.println(ans);
+        sc.close();
+    }
+}
